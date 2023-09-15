4972a2ded2d2d57b7d2990314c3b0e89dd5d8779
ed-w-ds <e.lisovskis@se22.qmul.ac.uk>
2023-09-15 10:06:01 +0100

    test update

--- cypress/e2e/bloglist.cy.js
+++ cypress/e2e/bloglist.cy.js
@@ -1,10 +1,4 @@
 /* eslint no-undef: 0 */
-// describe('template spec', () => {
-//     it('passes', () => {
-//         cy.visit('https://example.cypress.io')
-//     })
-// })
-
 // cy.get('[data-cy="submit"]').click()
 // and cy.contains('Submit').click()
 // is better than using #
@@ -13,33 +7,22 @@ describe('Blog app', function() {
 
     beforeEach(function() {
         cy.request('POST', `${Cypress.env('BACKEND')}/api/testing/reset`)
-        console.log(`${Cypress.env('BACKEND')}/api/testing/reset`)
-
-        cy.visit('')
-
-        cy.get('#username-input-signup').type('mluukkai')
-        cy.get('#name-input-signup').type('Matti Luukkainen')
-        cy.get('#password-input-signup').type('salainen')
-        cy.get('#signUp-button').click()
+        cy.signUpBackend({ username: 'mluukkai', name: 'Matti Luukkainen', password: 'salainen' })
     })
 
     it('Login form is shown', function() {
+        cy.visit('')
         cy.contains('login')
     })
 
     describe('Login', function() {
         it('succeeds with correct credentials', function() {
-            cy.get('#username-input').type('mluukkai')
-            cy.get('#password-input').type('salainen')
-            cy.get('#login-button').click()
-
+            cy.loginFrontend({ username: 'mluukkai', password: 'salainen' })
             cy.contains('Matti Luukkainen logged-in')
         })
 
         it('fails with wrong credentials', function() {
-            cy.get('#username-input').type('mluukkai')
-            cy.get('#password-input').type('wrong')
-            cy.get('#login-button').click()
+            cy.loginFrontend({ username: 'mluukkai', password: 'wrong' })
 
             cy.get('#notification')
                 .should('contain', 'Wrong credentials')
@@ -52,14 +35,8 @@ describe('Blog app', function() {
             cy.request('POST', `${Cypress.env('BACKEND')}/api/testing/reset`)
             cy.visit('')
 
-            cy.get('#username-input-signup').type('mluukkai')
-            cy.get('#name-input-signup').type('Matti Luukkainen')
-            cy.get('#password-input-signup').type('salainen')
-            cy.get('#signUp-button').click()
-
-            cy.get('#username-input').type('mluukkai')
-            cy.get('#password-input').type('salainen')
-            cy.get('#login-button').click()
+            cy.signUpBackend({ username: 'mluukkai', name: 'Matti Luukkainen', password: 'salainen' })
+            cy.loginBackend({ username: 'mluukkai', password: 'salainen' })
         })
 
         it('A blog can be created and exists', function() {
@@ -76,73 +53,32 @@ describe('Blog app', function() {
 
         describe('and a blog exists', function() {
             beforeEach(function() {
-                cy.contains('new blog').click()
-                cy.get('#title-input').type('a blog created by cypress')
-                cy.get('#author-input').type('cypress')
-                cy.get('#url-input').type('www.cypress.com')
-                cy.get('#submit-button').click()
-
-                cy.contains('a blog created by cypress').click()
+                cy.loginBackend({ username: 'mluukkai', password: 'salainen' })
+                cy.createBlog({ title: 'a blog created by cypress', author: 'cypress', url: 'www.cypress.com' })
             })
 
             it('the blog can be liked', function() {
-                cy.contains('a blog created by cypress || Author').click()
+                cy.contains('a blog created by cypress ||').click()
                 cy.contains('Likes: 0')
                 cy.contains('like').click()
                 cy.contains('Likes: 1')
             })
 
             it('the blog can be deleted by the user that created it', function() {
-                cy.contains('a blog created by cypress || Author').click()
+                cy.contains('a blog created by cypress ||').click()
                 cy.contains('remove').click()
                 cy.get('html').should('not.contain', 'a blog created by cypress')
-
-                // opt. include logout and login with another person who can't delete a blog
-                // cy.contains(logout).click()
-            })
-
-            it('the blog cannot be deleted by another user', function() {
-                cy.contains('logout').click()
-                // const user = {
-                //     name: 'Matti 2',
-                //     username: 'mluukkai2',
-                //     password: 'salainen'
-                // }
-                cy.get('#username-input-signup').type('mluukkai')
-                cy.get('#name-input-signup').type('Matti Luukkainen')
-                cy.get('#password-input-signup').type('salainen')
-                cy.get('#signUp-button').click()
-
-                // cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
-
-                // cy.login({ username: 'mluukkai2', password: 'salainen' })
-
-                cy.get('#username-input').type('mluukkai')
-                cy.get('#password-input').type('salainen')
-                cy.get('#login-button').click()
-
-                cy.contains('a blog created by cypress || Author').click()
-                cy.contains('remove').click()
-                cy.get('html').should('contain', 'a blog created by cypress')
             })
         })
 
-        describe.only('and multiple blogs exist', function() {
+        describe('and multiple blogs exist', function() {
             beforeEach(function() {
-                cy.contains('new blog').click()
-                cy.get('#title-input').type('The title with the most likes')
-                cy.get('#author-input').type('cypress')
-                cy.get('#url-input').type('www.cypress.com')
-                cy.get('#submit-button').click()
-
-                cy.contains('new blog').click()
-                cy.get('#title-input').type('The title with the second most likes')
-                cy.get('#author-input').type('cypress')
-                cy.get('#url-input').type('www.cypress.com')
-                cy.get('#submit-button').click()
+                cy.createBlog({ title: 'The title with the second most likes', author: 'cypress', url: 'www.cypress.com' })
+                cy.createBlog({ title: 'The title with the most likes', author: 'cypress', url: 'www.cypress.com' })
+                cy.createBlog({ title: 'The title with the third most likes', author: 'cypress', url: 'www.cypress.com' })
             })
 
-            it.only('blogs are ordered by likes', function() {
+            it('blogs are ordered by likes', function() {
                 cy.contains('The title with the most likes')
                     .click()
                 cy.get('#like-btn')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,20 +24,55 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add('login', ({ username, password }) => {
-    cy.request('POST', `${Cypress.env('BACKEND')}/login`, {
-        username: 'mluukkai', password: 'salainen'
+// Cypress.Commands.add('login', ({ username, password }) => {
+//     cy.request('POST', `${Cypress.env('BACKEND')}/login`, {
+//         username: 'mluukkai', password: 'salainen'
+//     }).then(response => {
+//         localStorage.setItem('loggedNoteappUser', JSON.stringify(response.body))
+//         cy.visit('')
+//     })
+// })
+
+Cypress.Commands.add('signUpFrontend', ({ username, name, password }) => {
+    cy.visit('')
+    cy.get('#username-input-signup').type(username)
+    cy.get('#name-input-signup').type(name)
+    cy.get('#password-input-signup').type(password)
+    cy.get('#signUp-button').click()
+})
+
+Cypress.Commands.add('signUpBackend', ({ username, name, password }) => {
+    cy.request('POST', `${Cypress.env('BACKEND')}/api/testing/reset`)
+    cy.request({
+        url: `${Cypress.env('BACKEND')}/api/users`,
+        method: 'POST',
+        body: { username: username, name: name, password: password }
+    })
+})
+
+Cypress.Commands.add('loginBackend', ({username, password}) => {
+    cy.request({
+        url: `${Cypress.env('BACKEND')}/api/login`,
+        method: 'POST',
+        body: { username: username, password: password }
     }).then(response => {
         localStorage.setItem('loggedNoteappUser', JSON.stringify(response.body))
         cy.visit('')
     })
 })
 
+Cypress.Commands.add('loginFrontend', ({username, password}) => {
+    cy.visit('')
+    cy.get('#username-input').type(username)
+    cy.get('#password-input').type(password)
+    cy.get('#login-button').click()
+})
+
 Cypress.Commands.add('createBlog', ({ title, author, url }) => {
     cy.request({
-        url: `${Cypress.env('BACKEND')}/blogs`,
+        url: `${Cypress.env('BACKEND')}/api/blogs`,
         method: 'POST',
-        body: { title, author, url },
+        body: { title, author, url, likes: 0, user: JSON.parse(localStorage.getItem('loggedNoteappUser')).id },
         headers: {
             'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedNoteappUser')).token}`
         }
